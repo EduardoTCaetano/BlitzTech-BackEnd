4fccce87a40c8193c1f6634bdace67fb807f65b1
Eduardo Timossi Caetano <eduardotimossi1101@hotmail.com>
2024-06-11 22:07:56 -0300

    Atualizando PUT e adicionando DELETE

--- BlitzTech.Application/Controllers/CategoryController.cs
+++ BlitzTech.Application/Controllers/CategoryController.cs
@@ -3,7 +3,6 @@ using BlitzTech.Data.Mapping;
 using BlitzTech.Data.Migrations;
 using BlitzTech.Domain.Dtos.Category;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace BlitzTech.Application.Controllers
 {
@@ -25,7 +24,7 @@ namespace BlitzTech.Application.Controllers
             return Ok(categoryModel);
         }
 
-        [HttpGet("{id:guid}")]
+        [HttpGet("{id}")]
         public IActionResult GetById([FromRoute] Guid id)
         {
             var categoryModel = _context.Categories.Find(id);
@@ -47,7 +46,8 @@ namespace BlitzTech.Application.Controllers
             return CreatedAtAction(nameof(GetById), new { id = categoryModel.Id }, AutoMapperProfiles.ToCategoryDto(categoryModel));
         }
 
-        [HttpPut("{id:guid}")]
+        [HttpPut]
+        [Route("{id}")]
         public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateCategoryRequestDto updateDto)
         {
             var categoryModel = _context.Categories.FirstOrDefault(x => x.Id == id);
@@ -56,28 +56,28 @@ namespace BlitzTech.Application.Controllers
             {
                 return NotFound("Id não encontrado :( ");
             }
-
-            // Update the properties
             categoryModel.Description = updateDto.Description;
             categoryModel.IsActive = updateDto.IsActive;
 
-            try
-            {
-                _context.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!_context.Categories.Any(e => e.Id == id))
-                {
-                    return NotFound("Id não encontrado :( ");
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
+            _context.SaveChanges();
             return Ok(categoryModel.ToCategoryDto());
         }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var categoryModel = _context.Categories.FirstOrDefault(x => x.Id == id);
+
+            if (categoryModel == null)
+            {
+                return NotFound("Id não encontrado :( ");
+            }
+
+            _context.Categories.Remove(categoryModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
     }
 }
